b7d1799beac7dd0194f7e78011d1702fbaacd17b
audzms2776 <diddydwns135@gmail.com>
2018-07-22 23:52:25 +0900

    Update app.py

--- deploy-server/app.py
+++ deploy-server/app.py
@@ -6,10 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def hook():
     
+    # find docker id 
     prev_id = subprocess.check_output(['sudo', 'docker', 'ps'],  universal_newlines=True).split('\n')[1].split('        ')[0]
     
+    # stop docker ps
     subprocess.call(['sudo', 'docker', 'stop', prev_id])
     subprocess.call(['sudo', 'docker', 'rm', prev_id]) 
+    
+    # rebuild docker image and start
     subprocess.call(['sudo', 'docker', 'build', '--no-cache', '-t',  'HbServer', '/home/ec2-user/HB-docker'])
     subprocess.call(['sudo', 'docker', 'run', '-d', '-p', '8888:3000', 'HbServer'])
 
